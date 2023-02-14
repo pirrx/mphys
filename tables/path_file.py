f00fb09ded0d02f91b30e0c88d74f78846cd8094
Aleksandra Bochenek <aleksandra.bochenek@student.manchester.ac.uk>
2023-02-14 14:18:29 +0000

    point distribution

---
+++ tables/path_file.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 14 12:27:45 2023
+
+@author: aleks
+"""
+
+RESIDUALS_PATH = "C:\\Users\\aleks\\OneDrive\\Dokumenty\\mphys\\colden_noise_fwhm_temp\\residuals\\"
+TABLES_PATH = "C:\\Users\\aleks\\OneDrive\\Dokumenty\\mphys\\tables\\"
